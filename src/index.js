d1adaa0da9ade12a2e318a3a3232315eda7477c7
Oleg Bukalov <60820640+OlegBukalov@users.noreply.github.com>
2020-03-01 16:28:56 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function toReadable (number) {
-  
+    let arr = number.toString().split(''), str = '', i=0;
+    let numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let numbersD= ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    if (number == 0) {
+        return 'zero';
+    }    
+    if (arr.length == 3) {
+        str += numbers[arr[i]]+' hundred ';
+        i++;
+    }
+    if (arr.length == 2 || i>0) {
+        if (arr[i] == 0) {
+            i++;
+        }
+        else if (arr[i] == 1) {
+            return str + numbersD[arr[i+1]];
+        }
+        else if (arr[i] == 2){
+            str += 'twenty '
+            i++;
+        }
+        else if (arr[i] == 4){
+            str += 'forty '
+            i++;
+        }
+        else {
+            let des = numbersD[arr[i]];
+            str += des.substr(0, des.length - 3)+'y ';
+            i++;
+        }
+    }
+    if (arr.length == 1 || i>0) {
+        if (arr[i] == 0) {
+            return str.substr(0, str.length-1);
+        }
+        str += numbers[arr[i]];;
+    }
+    return str;
 }
